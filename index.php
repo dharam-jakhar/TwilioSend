dfc58624855ec6db0e64b497151763d523be82e1
dharam-jakhar <36055801+dharam-jakhar@users.noreply.github.com>
2018-09-30 21:00:35 -0400

    third commit

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ require_once 'vendor/autoload.php';
  
 use Twilio\Rest\Client; 
  
-$sid    = "ACbd13b2e21b954d260beda885c432bbd9"; 
-$token  = "fb0e44a0fc7eaa1e80c797f891d30374"; 
+$sid    = "########"; //Use your sid
+$token  = "####"; //Use your token
 $twilio = new Client($sid, $token); 
  
 $message = $twilio->messages 
-                  ->create("+19173490168", // to 
+                  ->create("xxxxxxx", // to 
                            array( 
-                               "from" => "+18507064915",       
+                               "from" => "xxxxxx",       
                                "body" => "Hello from Dharam Jakhar CS 643 Fall 2018" 
                            ) 
                   ); 
